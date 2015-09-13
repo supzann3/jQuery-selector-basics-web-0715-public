1ed3dd58e9848f767fc537c65648c5250a35df23
supzann3 <susan.hehe01@gmail.com>
2015-09-13 19:29:49 -0400

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -7,4 +7,23 @@ $(document).ready(function(){
   checkboxInputSelector();
 });
 
+function h1Selector() {
+  return $("#content h1");
+}
+
+function liInOlSelector() {
+  return $("ol li");
+}
+
+function linkSelector() {
+  return $('#box4.box5 a[href$="#"]');
+}
+
+function imageSelector() {
+  return $("img[alt='cat sleeping']");
+}
+
+function checkboxInputSelector() {
+  return $("form input:checkbox").wrap("<span class='pinky'></span>");
+}
 // declare your functions here...
